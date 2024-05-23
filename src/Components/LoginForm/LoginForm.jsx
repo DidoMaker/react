5067ec035fcc9e1045885477f3a319c6e1740fe9
giang <gnaig1510@gmail.com>
2024-05-23 16:00:46 +0700

    LoginForm

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,43 +1,74 @@
-import React from "react";
+import React, { Component, Fragment } from 'react';
 import './LoginForm.css';
 import { FaUser, FaLock } from "react-icons/fa";
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css"></link>
 
-const LoginForm = () => {
-    return (
-        <div className="wrapper">
-            <form action="">
-                 <h1>Login</h1>
-                 <div className="input-box">
-                    <input type="text" placeholder="Username" required/>
-                    <FaUser className="icon"/>
-                 </div>
-                 <div className="input-box">
-                    <input type="password" placeholder="Password" required/>
-                    <FaLock className="icon"/>
+class LoginForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        }
+    }
+    handleonChangeUsername = (event) =>{
+        this.setState({
+            username: event.target.value
+        })
+    }
+    handleonChangePassword = (event) =>{
+        this.setState({
+            password: event.target.value
+        })
+    }
+    handleonLogin = () => {
+        console.log('username:' + this.state.username)
+        console.log('password: '+this.state.password)
+    }
+    render(){
+        return (
+            <div className="wrapper">
+                <form action="">
+                    <h1>Login</h1>
+                    <div className="input-box">
+                        <input type="text" placeholder="Username" required
+                        value={this.state.username}
+                        onChange={(event) => this.handleonChangeUsername(event)}
+                        />
+                        <FaUser className="icon"/>
+                    </div>
+                    <div className="input-box">
+                        <input type="password" placeholder="Password" required
+                        value={this.state.password}
+                        onChange={(event) => this.handleonChangePassword(event)}
+                        />
+                        <FaLock className="icon"/>
 
-                 </div>
-                 <div className="remember-forgot">
-                    <label>
-                        <input type="checkbox" />Remember me
-                    </label>
-                    <a href="#">forgot password</a>
-                 </div>
-                 <button type="submit">login</button>
-                 
-                 <div className="register-link">
-                    <p>Don't have account? <a href="#">Register</a></p>
-                 </div>
-                 <div className="another-account">
-                    <span className="text center">Or login with</span>
-                 </div>
-                 <div className="social-link">
-                     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css"></link>
-                     <i class="fa-brands fa-google-plus-g google"></i>
-                     <i class="fa-brands fa-facebook-f facebook"></i>
-                 </div> 
-            </form>
-        </div>
-    );
+                    </div>
+                    <div className="remember-forgot">
+                        <label>
+                            <input type="checkbox" />Remember me
+                        </label>
+                        <a href="https://www.facebook.com/">forgot password</a>
+                    </div>
+                    <div className=''>
+                        <button type="submit" onClick= {() => {this.handleonLogin() }}>login</button>
+                    </div>
+                    
+                    <div className="register-link">
+                        <p>Don't have account? <a href="https://www.youtube.com/">Register</a></p>
+                    </div>
+                    <div className="another-account">
+                        <span className="text center">Or login with</span>
+                    </div>
+                    <div className="social-link">
+                        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css"></link>
+                        <i class="fa-brands fa-google-plus-g google"></i>
+                        <i class="fa-brands fa-facebook-f facebook"></i>
+                    </div> 
+                </form>
+            </div>
+        );
+    }
 };
 export default LoginForm;
